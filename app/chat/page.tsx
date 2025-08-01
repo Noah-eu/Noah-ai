67b80d1297ea60bfadb0589b75d15f281ba8fad6
Noah-eu <david.eder78@gmail.com>
2025-08-01 09:13:33 +0200

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,4 @@
 import Chat from "./Chat";
-
 export default function Page() {
-  return <div style={{ background: "lime", color: "red", fontWeight: "bold", fontSize: 30 }}>TESTUJU VSTUP!!!</div>
+  return <Chat />;
 }
-
